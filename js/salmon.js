9b485283e20f126fd2bb91177b75469e5c3a4763
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-04-03 18:10:36 -0500

    commit

--- js/salmon.js
+++ js/salmon.js
@@ -1,2 +1,10 @@
+//google fonts code//
+
 WebFont.load({ google: { families: ['Trade Winds', 'Roboto'] } });
 
+
+// Menu nav bar//
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
