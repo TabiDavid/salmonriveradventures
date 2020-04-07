216d21a44394ca5ce3247709c6a8abaeed6c8c60
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-04-07 11:33:52 -0500

    commit

--- js/salmon.js
+++ js/salmon.js
@@ -40,7 +40,7 @@ weather.temperature = {
 }
 
 const KELVIN = 273;
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "aee52151b6cc7c7556030fdb68db7347";
 
 if('geolocation' in navigator){
     navigator.geolocation.getCurrentPosition(setPosition, showError);
