e7a0763135255fd082f26a1fa8ed8d809d37e845
TabiDavid <55420549+tabiDO@users.noreply.github.com>
2020-04-08 22:38:50 -0500

    commit

--- js/salmon.js
+++ js/salmon.js
@@ -12,7 +12,9 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 
 //Json object//
-function guideRiver(){
+
+
+function John(){
 
     var text = '{"employees":[' +
     '{"firstName":"John","lastName":"Doe", "age":"22", "Certificate":"Tourism","experience":"5 years", "email":"John@email.com","description":"I love to work with a lot of people and teaching and helping them to know the marvelous creation that this world has."},' +
@@ -26,15 +28,36 @@ function guideRiver(){
     obj.employees[0].Certificate + " " + obj.employees[0].experience + " " + obj.employees[0].email + " " + obj.employees[0].description;
     
 
+};
+
+function Anna(){
+
+    var text = '{"employees":[' +
+    '{"firstName":"John","lastName":"Doe", "age":"22", "Certificate":"Tourism","experience":"5 years", "email":"John@email.com","description":"I love to work with a lot of people and teaching and helping them to know the marvelous creation that this world has."},' +
+    '{"firstName":"Anna","lastName":"Smith", "age":"25", "Certificate":"Tourism","experience":"4 years", "email":"Anna@email.com", "description":"If you are looking for someone who really likes to walk and teach you about plan and animal I am the right person."},' +
+    '{"firstName":"Peter","lastName":"Jones", "age":"40", "Certificate":"Tourism","experience":"3 years", "email":"Peter@email.com", "description":"Explere and have adventure is something that I love since I was a child if you have the same desire just call me and spend a fun time." }]}';
+    
+    obj = JSON.parse(text);
+    
 
     document.getElementById("outPut2").innerHTML =
     obj.employees[1].firstName + " " + obj.employees[1].lastName + " " + obj.employees[1].age + " " + 
     obj.employees[1].Certificate + " " + obj.employees[1].experience + " " + obj.employees[1].email + " " + obj.employees[1].description;
     
+
+};
+
+function Peter(){
+
+    var text = '{"employees":[' +
+    '{"firstName":"John","lastName":"Doe", "age":"22", "Certificate":"Tourism","experience":"5 years", "email":"John@email.com","description":"I love to work with a lot of people and teaching and helping them to know the marvelous creation that this world has."},' +
+    '{"firstName":"Anna","lastName":"Smith", "age":"25", "Certificate":"Tourism","experience":"4 years", "email":"Anna@email.com", "description":"If you are looking for someone who really likes to walk and teach you about plan and animal I am the right person."},' +
+    '{"firstName":"Peter","lastName":"Jones", "age":"40", "Certificate":"Tourism","experience":"3 years", "email":"Peter@email.com", "description":"Explere and have adventure is something that I love since I was a child if you have the same desire just call me and spend a fun time." }]}';
+    
+    obj = JSON.parse(text);
     
     document.getElementById("outPut3").innerHTML =
     obj.employees[2].firstName + " " + obj.employees[2].lastName + " " + obj.employees[2].age + " " + 
     obj.employees[2].Certificate + " " + obj.employees[2].experience + " " + obj.employees[2].email + " " + obj.employees[2].description;
         
-
 };
